187868ed14a3c322de14e0754bd72f563039e762
Pavel Keyzik <pavel.keyzik@gmail.com>
2016-10-21 13:16:52 +0300

    Final commit. JS transfer to folder

---
+++ sites/HBudget/js/script.js
@@ -0,0 +1,10 @@
+function showPopUp(id){
+    display = document.getElementById(id).style.display;
+
+	if(display=='block'){
+		document.getElementById(id).style.display='none';
+	}
+	else{
+		document.getElementById(id).style.display='block';
+	}
+}
