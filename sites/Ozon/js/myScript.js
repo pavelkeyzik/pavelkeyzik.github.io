a31b4731161246d773faaa3ff4452fcd3fabec52
Pavel Keyzik <pavel.keyzik@gmail.com>
2016-10-23 21:59:25 +0300

    Full main page 1

---
+++ sites/Ozon/js/myScript.js
@@ -0,0 +1,14 @@
+function openBox(id)
+{
+	display = document.getElementById(id).style.display;
+
+	if(display == 'block')
+	{
+		document.getElementById(id).style.display='none';
+	}
+	else
+	{
+		document.getElementById(id).style.display='block';
+	}
+
+}
