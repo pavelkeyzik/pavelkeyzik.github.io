721007f48ec7e5fbeaf5af63ab959d61cd50f58e
Pavel Keyzik <pavel.keyzik@gmail.com>
2016-10-26 19:55:39 +0300

    Final Relase 1

--- sites/Ozon/js/myScript.js
+++ sites/Ozon/js/myScript.js
@@ -1,14 +1,26 @@
-function openBox(id)
-{
+function openAndCloseBox(id) {
 	display = document.getElementById(id).style.display;
-
-	if(display == 'block')
-	{
-		document.getElementById(id).style.display='none';
-	}
-	else
-	{
-		document.getElementById(id).style.display='block';
-	}
-
+	document.getElementById(id).style.display = (display == "flex") ? "none" : "flex";
 }
+function openAndCloseSelect(id) {
+	display = document.getElementById(id).style.display;
+	document.getElementById(id).style.display = (display == "block") ? "none" : "block";
+}
+
+// Хотел сделать табы вверху, но не успел. Код снизу фигня, но я JS ещё даже не изучал
+
+// function openTab(id) {
+// 	display = document.getElementById(id).style.display;
+// 	if(display == "none")
+// 	{
+// 		document.getElementById(id).style.display = "block";
+// 		if(id == "formForTabRight")
+// 		{
+// 			document.getElementById("formForTabLeft").style.display = "none"; 
+// 		}
+// 		else
+// 		{
+// 			document.getElementById("formForTabRight").style.display = "none"; 
+// 		}
+// 	}
+// }
