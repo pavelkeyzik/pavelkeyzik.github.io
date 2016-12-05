10fe7dcbe2c50e5ae6ddd2f177681a14c4106696
Pavel <pavel.keyzik@gmail.com>
2016-12-06 00:04:14 +0300

    Add ng-route

--- sites/AngularJS/js/app/app.config.js
+++ sites/AngularJS/js/app/app.config.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('try').
-  config(function(){
-    
+  config(function($routeProvider){
+    $routeProvide
+     .when("/", {
+        templateUrl : "index.html"
+    })
+    .when("/article", {
+        templateUrl : "templates/redappartament-info.html"
+    })
   });
--- sites/AngularJS/js/app/app.module.js
+++ sites/AngularJS/js/app/app.module.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('try', [
+	'ngRoute',
 	'appartamentsList',
 	'appartamentInfo'
 ]);
